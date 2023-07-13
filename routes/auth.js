0657fa527f85a4f183af801284f888db7bfa292b
shaheer-cowlar <shaheer.khan@cowlar.com>
2023-07-13 12:29:36 +0500

    validation of email and password and local storage of users

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,40 @@
 const router = require("express").Router();
+const { users } = require("../db")
+const { check,validationResult } = require("express-validator")
+router.post('/signup',
+[
+  check("email","Please provide a valid email").isEmail(),
+  check("password","Please provide a password with a minimum of 6 characters").isLength({
+    min: 6
+  })
+],
+(req, res) => {
+  const { password, email } = req.body;
 
-router.post('/signup',(req,res)=>{
-   const {password,email} = req.body;
-    console.log(password,email);
-    res.send("Auth route working");
-})
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array()
+    });
+  }
+
+  const userExists = users.find((user) => {
+    return user.email === email;
+  });
+
+  if (userExists) {
+    return res.status(400).json({
+      "errors": [
+        {
+          "msg": "This user already exists"
+        }
+      ]
+    });
+  }
+
+  console.log(password, email);
+  res.send("Validation passed");
+});
 
 module.exports = router;
