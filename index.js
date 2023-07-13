bd8ba1e6ae7b2aad95816d1f4612b15966065d5f
shaheer-cowlar <shaheer.khan@cowlar.com>
2023-07-13 11:33:23 +0500

    email and password succesfully obtained

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const auth = require("./routes/auth");
+
 
 const app = express()
+app.use(express.json())
+app.use("/auth",auth)
+
 
 app.listen(5000,()=>{
     console.log('Now running on port 5000')
